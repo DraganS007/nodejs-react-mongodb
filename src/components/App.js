2223ad7ee785b83dfd55805507dc2166777ab1b5
Dragan Siljanoski <dragan.siljanoski@hotmail.com>
2017-01-16 02:06:22 -0500

    Actual Day 1 progress

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,14 @@ import ContestPreview from './ContestPreview';
 
 class App extends React.Component{
 	state = {
-		pageHeader: 'Naming Contests'
+		pageHeader: 'Naming Contests',
+		contests : this.props.initialContests
 	};
 	componentDidMount(){
+		
 	}
 	componentWillUnmount(){
+
 	}
 
 	render(){
@@ -16,8 +19,8 @@ class App extends React.Component{
 			<div className="App">
 				<Header message={this.state.pageHeader} />
 				<div>
-					{this.props.contests.map(contest =>
-						<ContestPreview {...contest}/>
+					{this.state.contests.map(contest =>
+						<ContestPreview key={contest.id} {...contest}/>
 					)}
 				</div>
 			</div>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import data from './testData';
 import App from './components/App';
 
-console.log(data);
 
 ReactDOM.render(
-	<App contests =  {data.contests}/>,
+	<App initialContests={window.initialData.contests}/>,
 	document.getElementById('root')
 );
+
+
